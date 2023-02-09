9786e26e2bd72f0ebbbf02ef837166faccdbb996
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-02-08 23:13:23 -0300

    commiting from the git-in-nodejs package

--- src/commandTests.js
+++ src/commandTests.js
@@ -1,11 +1,11 @@
-const GitController = require("./gitController/gitController")
+const GitController = require("git-in-nodejs")
 
 async function run(){
     let repoPath = process.cwd();
     let branch = "main";
 
     const thisRepo = new GitController({repoPath, branch});
-    const message = "message var added"
+    const message = "commiting from the git-in-nodejs package"
 
     await thisRepo.commitAll(message);
     await thisRepo.push();
